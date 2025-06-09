4f541c0d0c38d7440793afb35746572562502e34
Willa Mayrose <francesmay775@gmail.com>
2025-06-09 11:39:36 -0500

    Initial Commit

---
+++ StormSafe/StormSafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StormSafeApp.swift
+//  StormSafe
+//
+//  Created by Scholar on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct StormSafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

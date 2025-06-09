7ff98fa6c9adda24476a6d0f2ab21bd4109b995b
Willa Mayrose <francesmay775@gmail.com>
2025-06-09 15:07:32 -0500

    initial home screen

--- StormSafe/ContentView.swift
+++ StormSafe/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Group {
+            VStack {
+                Image(systemName: "house")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Home")
+            }
+            .padding()
         }
-        .padding()
     }
 }
 

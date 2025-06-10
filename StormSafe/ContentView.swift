0176d912c8686fc93aa00af6666ff9515e697a67
Lia <lworland28@stjosephacademy.org>
2025-06-10 10:37:17 -0500

    added storm pick kand stormsafe

--- StormSafe/ContentView.swift
+++ StormSafe/ContentView.swift
@@ -10,32 +10,37 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationStack {
-            Text("Content")
-            NavigationLink(destination: DonationPage()) {
-                Text("For Donating")
-            } // end NavLinkm,
-            NavigationLink(destination: SupportForm()) {
-                Text("Support Form")
+            ZStack {
+                Image("storm")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fill)
+                    .padding(.bottom)
+                    .frame(width: 300.0)
+                Text("StormSafe")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.black)
+                    .padding([.top, .trailing], 120.0)
             }
-          NavigationLink(destination:
-            Text( " Earthquakes")) {
-            Text ("Click Me for Information on Earthquakes🌎 ")
+                VStack {
+                    Text("Welcome")
+                    NavigationLink(destination: DonationPage()) {
+                        Text("For Donating")
+                    } // end NavLinkm,
+                    NavigationLink(destination: SupportForm()) {
+                        Text("Support Form")
                     }
-           NavigationLink(destination: Text("Torandoes")) {
+                    NavigationLink(destination:
+                                    Text( " Earthquakes")) {
+                        Text ("Click Me for Information on Earthquakes")
+                    }
+                    NavigationLink(destination: Text("Torandoes")) {
                         Text("Click Me for Information on Torandos")
                     }
+                }
         } // end NavStack
-        Group {
-            VStack {
-                Image(systemName: "house")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Home2")
-            } // end VStack
-            .padding()
         } // end Group
     } // end var body
-} // end struct
 
 #Preview {
     ContentView()

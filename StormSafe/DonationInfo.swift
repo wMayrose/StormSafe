15aa2417f1e63a9f89b29106ff825b44fc50a672
Willa Mayrose <francesmay775@gmail.com>
2025-06-10 10:14:14 -0500

    donation info built

---
+++ StormSafe/DonationInfo.swift
@@ -0,0 +1,18 @@
+//
+//  DonationInfo.swift
+//  StormSafe
+//
+//  Created by Scholar on 6/10/25.
+//
+
+import SwiftUI
+
+struct DonationInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DonationInfo()
+}
